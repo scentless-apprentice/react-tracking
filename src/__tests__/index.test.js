f76a4113eaaa1aa56a9c9ce099828982e3563de4
Aneudy Abreu <aneudy.abreu@nytimes.com>
2017-02-03 12:49:37 -0500

    updated test based on the latest changes

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,7 +3,7 @@ const example = require('../example');
 
 describe('index', () => {
   it('validates against example data', () => {
-    var errors = validate(example);
-    expect(!errors).toBe(true, errors);
+    var errors = validate({data: example});
+    expect(!errors.length).toBe(true, errors);
   });  
 });
