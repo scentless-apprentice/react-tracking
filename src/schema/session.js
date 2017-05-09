c121f8ca7dca824e12a91ba5c0d7deb2cb69838f
Aneudy Abreu <aneudy.abreu@nytimes.com>
2017-05-09 10:53:21 -0400

    added Temporary Descriptions to the session chunk

--- src/schema/session.js
+++ src/schema/session.js
@@ -2,5 +2,22 @@
  * Contains data pertaining to the user's session.
  */
 module.exports = {
-  // ...
+  'desc': 'Contains data pertaining to the NYT user\'s session.',
+
+  // disallow properties not explicilty listed
+  additionalProperties: false,
+
+  // properties that are required (for `user`)
+  // required: ['type'],
+
+  properties: {
+    'meterCount': {
+      type: 'number'
+      description: 'A value that indicates the total number of metered NYT content assets consumed during the user session.',
+    }
+    'isLoggedIn': {
+      type: 'boolean'
+      description: 'A flag that indicates whether a user session is authenticated.',
+    }
+  }    
 };
