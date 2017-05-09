7c6fa5bfe05ff2c8e2221507030b831ab8445088
Aneudy Abreu <aneudy.abreu@nytimes.com>
2017-05-09 10:49:59 -0400

    added Descriptions to the user chunk

--- src/schema/user.js
+++ src/schema/user.js
@@ -2,6 +2,8 @@
 * User specific data.
 */
 module.exports = {
+  'description': 'Describes basic attributes of the NYT user.',
+
   // disallow properties not explicilty listed
   additionalProperties: false,
 
@@ -12,16 +14,21 @@ module.exports = {
     /**
      * The NYT Registration ID of the user.
      */
-    regiId: { type: 'string' },
+    regiId: {
+      type: 'string',
+      description: 'The NYT registration ID of the NYT user.',
+    },
 
     /**
      * Type of NYT user.
      */
     type: {
       enum: ['anon', 'regi', 'sub'],
+      description: 'A value that indicates the type of NYT user',
     },
 
     subscriptions: {
+      description: 'TBD',
       items: {
         type: 'object'
       }
@@ -30,7 +37,10 @@ module.exports = {
     /**
      * Segments under 'wat' in NYT-D cookie
      */
-    watSeg: { type: 'string' },
+    watSeg: {
+      type: 'string',
+      description: 'segments under ‘wat’ in the NYT-D cookie',
+    },
 
   }, // end user.properties
 };
