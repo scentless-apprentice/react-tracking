7945695676abbb479b151024f8426a81a3d3f79d
Aneudy Abreu <aneudy.abreu@nytimes.com>
2017-05-09 10:56:29 -0400

    added Description to referrer chunk

--- src/schema/referrer.js
+++ src/schema/referrer.js
@@ -9,10 +9,15 @@ module.exports = {
   // properties that are required (for `referrer`)
   required: ['url'],
 
+  description: 'Contains data about external referrers, internal referrers and module interactions pertaining to the NYT user\'s session.',
+
   properties: {
     /**
      * Referring URL
      */
-    url: { format: 'uri' },
+    url: {
+      format: 'uri'
+      description: 'A URL of previous page or document.referrer',
+    },
   }, // end referrer.properties
 };
