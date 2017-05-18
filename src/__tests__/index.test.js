fbce897fe41fa176539bc3d563f5048b61f0f12b
Sunil Kapila <sunil.kapila@nytimes.com>
2017-05-18 12:28:04 -0400

    Add test for individual event

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,10 +1,19 @@
-const validate = require('../index');
+const validate = require('../index').validate;
 const example = require('../example');
 
 describe('index', () => {
   it('validates against example data', () => {
-    var result = validate(example);
+    const result = validate(example);
 
     expect(!result.errors).toBe(true, result.errors);
-  });  
+  });
+
+  it('validates individual video event data', () => {
+    const result = validate(
+      { video: example.video },
+      { individualEvents: true },
+    );
+
+    expect(!result.errors).toBe(true, result.errors);
+  });
 });
