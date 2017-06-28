0fbb054e775372bee979a7876583f2b8edb130a6
Eugene Wang <eugene.wang@nytimes.com>
2017-06-28 14:34:50 -0400

    more schema updates

--- src/schema/interaction.js
+++ src/schema/interaction.js
@@ -14,38 +14,6 @@ module.exports = {
     /**
      * The module interacted with.
      */
-    module: {
-      additionalProperties: false,
-      description: 'TBD',
-      properties: {
-        /**
-         * The name of the module interacted with.
-         * E.g. "see more"
-         */
-        name: {
-          type: 'string',
-          description: 'see more',
-        },
-
-        /**
-         * The context the module is rendered in.
-         * E.g. "block"
-         */
-        context: {
-          type: 'string',
-          description: 'block',
-        },
-
-        /**
-         * Module's label.
-         * E.g. "Top News", "Featured", "Opinion", etc.
-         */
-        label: {
-          type: 'string',
-          description: 'Top News|Featured|Opinion|News|Culture|etc...',
-        },
-      }, // end interaction.module.properties
-    }, // end interaction.module
 
     // Region to be defined
     region: {
--- src/schema/module.js
+++ src/schema/module.js
@@ -2,12 +2,38 @@
 * Module specific data.
 */
 module.exports = {
-  'description': 'Describes basic attributes of the NYT user.',
+  'description': 'Describes attributes of a module on the page',
 
   // disallow properties not explicilty listed
   additionalProperties: false,
 
   properties: {
+    /**
+     * The name of the module interacted with.
+     * E.g. "see more"
+     */
+    name: {
+      type: 'string',
+      description: 'see more',
+    },
+
+    /**
+     * The context the module is rendered in.
+     * E.g. "block"
+     */
+    context: {
+      type: 'string',
+      description: 'block',
+    },
+
+    /**
+     * Module's label.
+     * E.g. "Top News", "Featured", "Opinion", etc.
+     */
+    label: {
+      type: 'string',
+      description: 'Top News|Featured|Opinion|News|Culture|etc...',
+    },
 
   }, // end module.properties
 };
